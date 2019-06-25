0bd5c24773d0a31a57727ece54a560d4412005d2
onatakduman <onatakduman@gmail.com>
2019-06-25 13:13:47 +0300

    Migrating AndroidX. Google Maps Gradle setup.

--- app/src/main/java/com/example/candogan/todoexercise/MainActivity.java
+++ app/src/main/java/com/example/candogan/todoexercise/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.candogan.todoexercise;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 
 import java.util.ArrayList;
